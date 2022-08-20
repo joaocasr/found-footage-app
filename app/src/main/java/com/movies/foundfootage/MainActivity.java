1a45dcced302b124b5bba3e76a72617eef42b04c
João Castro <73347405+joaocasr@users.noreply.github.com>
2022-08-20 14:35:17 +0100

    add movies to database(watchlist section)

--- app/src/main/java/com/movies/foundfootage/MainActivity.java
+++ app/src/main/java/com/movies/foundfootage/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,10 +18,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.movies.foundfootage.Models.Movie;
 import com.movies.foundfootage.Interface.DatabaseHelper;
+import com.movies.foundfootage.Models.Movie;
 import com.movies.foundfootage.Interface.RecyclerViewInterface;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -33,150 +38,230 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewInter
     private EditText editText;
     private BottomNavigationView bottomNavigationView;
 
+    ArrayList<String> arrayList;
+    ArrayList<String> watchlistMovies;
 
     public List<Movie> movieList = new ArrayList<>();
+    private ArrayList<Movie> filteredList = new ArrayList<Movie>();
+    public List<String> likedMovies = new ArrayList<>();
+    public List<String> toWatchMovies = new ArrayList<>();
     RecyclerView recyclerView;
     final MovieSearchAdapter[] movieSearchAdapter = {new MovieSearchAdapter(movieList, this)};
     DatabaseHelper db;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
 
-            this.buildMovies();
-            movieSearchAdapter[0] = new MovieSearchAdapter(movieList,this);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-            editText = (EditText) findViewById(R.id.search_view);
-            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        this.buildMovies();
+        movieSearchAdapter[0] = new MovieSearchAdapter(movieList,this);
 
-            recyclerView = findViewById(R.id.recycler_view1);
-            bottomNavigationView = findViewById(R.id.bottom_navigation);
-            bottomNavigationView.setSelectedItemId(R.id.nav_home);
+        editText = (EditText) findViewById(R.id.search_view);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
-            spinner = (Spinner) findViewById(R.id.spinner);
+        recyclerView = findViewById(R.id.recycler_view1);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
 
-            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
-            recyclerView.setLayoutManager(layoutManager);
-            recyclerView.setHasFixedSize(true);
-            recyclerView.setAdapter(movieSearchAdapter[0]);
+        spinner = (Spinner) findViewById(R.id.spinner);
 
-            List<String> opcoes = Arrays.asList("<-none->","Alphabetic","IMDB Rate","Release");
-            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,R.layout.selected_item,opcoes);
-            arrayAdapter.setDropDownViewResource(R.layout.dropdown_item);
-            spinner.setAdapter(arrayAdapter);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(movieSearchAdapter[0]);
 
-            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                @Override
-                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                    if(i==1){
-                        Comparator<Movie> titleComparator = (m1,m2)->m1.compareTo(m2);
-                        Collections.sort(movieList,titleComparator);
-                        movieSearchAdapter[0] = new MovieSearchAdapter(movieList,MainActivity.this);
-                        recyclerView.setAdapter(movieSearchAdapter[0]);
-                    }
-                    if(i==2){
-                        Collections.sort(movieList,Movie.rateComparator);
-                        movieSearchAdapter[0] = new MovieSearchAdapter(movieList,MainActivity.this);
-                        recyclerView.setAdapter(movieSearchAdapter[0]);
-                    }
-                    if(i==3){
-                        Collections.sort(movieList,Movie.releaseComparator);
-                        movieSearchAdapter[0] = new MovieSearchAdapter(movieList,MainActivity.this);
-                        recyclerView.setAdapter(movieSearchAdapter[0]);
-                    }
+        List<String> opcoes = Arrays.asList("<-none->","Alphabetic","IMDB Rate","Release");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,R.layout.selected_item,opcoes);
+        arrayAdapter.setDropDownViewResource(R.layout.dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i==1){
+                    Comparator<Movie> titleComparator = (m1,m2)->m1.compareTo(m2);
+                    Collections.sort(movieList,titleComparator);
+                    movieSearchAdapter[0] = new MovieSearchAdapter(movieList,MainActivity.this);
+                    recyclerView.setAdapter(movieSearchAdapter[0]);
                 }
-
-                @Override
-                public void onNothingSelected(AdapterView<?> adapterView) {
-
+                if(i==2){
+                    Collections.sort(movieList,Movie.rateComparator);
+                    movieSearchAdapter[0] = new MovieSearchAdapter(movieList,MainActivity.this);
+                    recyclerView.setAdapter(movieSearchAdapter[0]);
                 }
-            });
-
-            //db = new DatabaseHelper(this);
-            //db.addMovie(R.drawable.poster1//);
-            //movieSearchAdapter = new MovieSearchAdapter(this,db.getAllMovies());
-
-
-            bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    switch (item.getItemId()){
-                        case R.id.nav_new_release:
-                            startActivity(new Intent(getApplicationContext(),ReleaseActivity.class));
-                            overridePendingTransition(0,0);
-                            return true;
-                        case R.id.nav_home:
-                            return true;
-                    }
-                    return false;
-                }
-            });
-
-
-            editText.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                    return;
-                }
-
-                @Override
-                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                    return;
-
-                }
-
-                @Override
-                public void afterTextChanged(Editable editable) {
-                    filter(editable.toString(),movieSearchAdapter);
-                }
-            });
-
-        }
-        private void filter(String s,MovieSearchAdapter [] adapter){
-            ArrayList<Movie> filteredList = new ArrayList<>();
-            for (Movie m : movieList){
-                if(m.getTitle().toLowerCase().contains(s.toLowerCase())){
-                    filteredList.add(m);
+                if(i==3){
+                    Collections.sort(movieList,Movie.releaseComparator);
+                    movieSearchAdapter[0] = new MovieSearchAdapter(movieList,MainActivity.this);
+                    recyclerView.setAdapter(movieSearchAdapter[0]);
                 }
             }
-            movieSearchAdapter[0].filteredMovies(filteredList);
-            //Toast.makeText(this,filteredList.get(0).getTitle(),Toast.LENGTH_LONG).show();
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        db = new DatabaseHelper(MainActivity.this);
+        //SQLiteDatabase sqLiteDatabase = db.getWritableDatabase();
+        //db.onUpgrade(sqLiteDatabase,5,6);
+        //db.deleteAll();
+        arrayList = db.getFavs();
+        watchlistMovies = db.getToWatch();
+        this.likedMovies = new ArrayList<>(arrayList);
+        this.toWatchMovies = new ArrayList<>(watchlistMovies);
+        //db.addMovie(R.drawable.poster1//);
+        //movieSearchAdapter = new MovieSearchAdapter(this,db.getAllMovies());
+
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_new_release:
+                        startActivity(new Intent(getApplicationContext(),ReleaseActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.nav_likes:
+                        startActivity(new Intent(getApplicationContext(),Likes.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.nav_watchlist:
+                        startActivity(new Intent(getApplicationContext(),Watchlist.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.nav_home:
+                        return true;
+                }
+                return false;
+            }
+        });
+
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                return;
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                return;
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                filter(editable.toString());
+            }
+        });
+
+        /*Adicionar filme à lista de filmes por ver*/
+        String watchlistMovie = getIntent().getStringExtra("watchlistTitle");
+
+        if(watchlistMovie!=null){
+            Toast.makeText(MainActivity.this,"Watchlist has been updated.",Toast.LENGTH_LONG).show();
+            db.addRemoveFav("#"+watchlistMovie);
+
         }
 
+    }
+    private void filter(String s){
+        filteredList.clear();
+        for (Movie m : movieList){
+            if(m.getTitle().toLowerCase().contains(s.toLowerCase())){
+                filteredList.add(m);
+            }
+        }
+        movieSearchAdapter[0].filteredMovies(filteredList);
+        //Toast.makeText(this,filteredList.get(0).getTitle(),Toast.LENGTH_LONG).show();
+    }
 
-        public void buildMovies(){
-            Movie m1 = new Movie(1,"Incantation",R.drawable.poster1,"2022","Horror","Six years ago, Li Ronan was cursed after breaking a religious taboo. Now, she must protect her daughter from the consequences of her actions.",6.2,1.50," Actors: Hsuan-yen Tsai, Mohamed Elgendy, Ying-Hsuan Kao, Sean Lin","Director: Kevin Ko");
-            Movie m2 = new Movie(2,"Paranormal Activity: Next of Kin",R.drawable.poster2,"2021","Horror, Mystery, Thriller","Margot, a young woman who was abandoned by her mother as a baby, travels to a secluded Amish community with a documentary film crew seeking answers about her mother and extended family.",5.2,1.38,"Actors: Emily Bader, Roland Buck III, Dan Lippert, Jaye Ayres-Brown","Director: William Eubank");
-            Movie m3 = new Movie(3,"The Visit",R.drawable.poster3,"2015","Horror, Mystery, Thriller","Two siblings become increasingly frightened by their grandparents' disturbing behavior while visiting them on vacation.",6.2,1.34," Actors: Olivia DeJonge, Ed Oxenbould, Deanna Dunagan, Peter McRobbie","Director: M. Night Shyamalan");
-            Movie m4 = new Movie(4,"Cannibal Holocaust",R.drawable.poster4,"1980","Adventure, Horror","During a rescue mission into the Amazon rainforest, a professor stumbles across lost film shot by a missing documentary crew.",5.8,1.35," Actors: Robert Kerman, Francesca Ciardi, Perry Pirkanen, Luca Barbareschi","Director: Ruggero Deodato");
 
-            movieList.add(m1);
-            movieList.add(m2);
-            movieList.add(m3);
-            movieList.add(m4);
+    public void buildMovies(){
+            int l=1;
+            StringBuilder sb = new StringBuilder();
+            InputStream inputStream = getResources().openRawResource(R.raw.moviedb);
+
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+                String line = null;
+                while((line = br.readLine()) != null) {
+                    String[] parte = line.split(";");
+                    String mDrawableName = sb.append("poster").append(l).toString();
+                    int imageID = getResources().getIdentifier(mDrawableName , "drawable", getPackageName());
+                    movieList.add(new Movie(l,parte[0],imageID,parte[1],parte[2],parte[3],Double.parseDouble(parte[4]),parte[5],Double.parseDouble(parte[6]),parte[7],parte[8]));
+                    // read next line
+                    sb = new StringBuilder();
+                    l++;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
 
     @Override
     public void onClick(int item) {
         Intent intent = new Intent(MainActivity.this,DetailedActivity.class);
-        intent.putExtra("TITLE",movieList.get(item).getTitle());
-        intent.putExtra("IMAGE",movieList.get(item).getPoster_path());
-        intent.putExtra("RELEASE",movieList.get(item).getRelease());
-        intent.putExtra("GENRES",movieList.get(item).getGenres());
-        intent.putExtra("PLOT",movieList.get(item).getPlot());
-        intent.putExtra("RATE",movieList.get(item).getRate());
-        intent.putExtra("DURATION",movieList.get(item).getTime());
-        intent.putExtra("ACTORS",movieList.get(item).getActors());
-        intent.putExtra("DIRECTOR",movieList.get(item).getDirectors());
-
+        if(filteredList.size()>0){
+            intent.putExtra("TITLE", filteredList.get(item).getTitle());
+            intent.putExtra("IMAGE", filteredList.get(item).getPoster_path());
+            intent.putExtra("RELEASE", filteredList.get(item).getRelease());
+            intent.putExtra("GENRES", filteredList.get(item).getGenres());
+            intent.putExtra("PLOT", filteredList.get(item).getPlot());
+            intent.putExtra("RATE", filteredList.get(item).getRate());
+            intent.putExtra("METASCORE", filteredList.get(item).getMetascore());
+            intent.putExtra("DURATION", filteredList.get(item).getTime());
+            intent.putExtra("ACTORS", filteredList.get(item).getActors());
+            intent.putExtra("DIRECTORS", filteredList.get(item).getDirectors());
+        }else {
+            intent.putExtra("TITLE", movieList.get(item).getTitle());
+            intent.putExtra("IMAGE", movieList.get(item).getPoster_path());
+            intent.putExtra("RELEASE", movieList.get(item).getRelease());
+            intent.putExtra("GENRES", movieList.get(item).getGenres());
+            intent.putExtra("PLOT", movieList.get(item).getPlot());
+            intent.putExtra("RATE", movieList.get(item).getRate());
+            intent.putExtra("METASCORE", movieList.get(item).getMetascore());
+            intent.putExtra("DURATION", movieList.get(item).getTime());
+            intent.putExtra("ACTORS", movieList.get(item).getActors());
+            intent.putExtra("DIRECTORS", movieList.get(item).getDirectors());
+        }
         startActivity(intent);
 
     }
 
-    //public List<Movie> getMovieList(){
-    //        return this.movieList.stream().map(Movie::clone).collect(Collectors.toList());
-   //}
+    @Override
+    public void onFavClick(int item) {
+        String title ="";
+        if(filteredList.size()>0) title = filteredList.get(item).getTitle();
+        else title = movieList.get(item).getTitle();
+        db.addRemoveFav(title);
+        //Toast.makeText(MainActivity.this,db.getFavs().get(0),Toast.LENGTH_LONG).show();
 
+    }
+
+    public List<Movie> getMovieList(){
+        List<Movie> l = new ArrayList<>();
+        for(String title :likedMovies){
+            for(Movie m : movieList){
+                if(m.getTitle().equals(title)){
+                    l.add(m.clone());
+                }
+            }
+        }
+        return l;
+    }
+
+    public List<Movie> getWatchList(){
+        List<Movie> l = new ArrayList<>();
+        for(String title :toWatchMovies){
+            for(Movie m : movieList){
+                if(m.getTitle().equals(title.substring(1))){
+                    l.add(m.clone());
+                }
+            }
+        }
+        return l;
+    }
 }

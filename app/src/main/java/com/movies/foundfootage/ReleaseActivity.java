c4fe3e32478237c77c8a661e233f8f9747c735fa
João Castro <73347405+joaocasr@users.noreply.github.com>
2022-09-13 23:35:59 +0100

    alteracao do construtor dos filmes

--- app/src/main/java/com/movies/foundfootage/ReleaseActivity.java
+++ app/src/main/java/com/movies/foundfootage/ReleaseActivity.java
@@ -13,7 +13,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.movies.foundfootage.Models.Movie;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -40,18 +39,6 @@ public class ReleaseActivity extends MainActivity {
         bottomNavigationView.setSelectedItemId(R.id.nav_new_release);
         recyclerView2 = findViewById(R.id.recycler_view2);
 
-        /*textView = findViewById(R.id.movietxt);
-
-        int currentyear = Calendar.getInstance().get(Calendar.YEAR);
-
-        StringBuilder stringBuilder = new StringBuilder();
-        for(Movie m : getMovieList()) {
-            if (currentyear == Integer.parseInt(m.getRelease())) {
-                stringBuilder.append("Movie: ").append(m.getTitle()).append("\n");
-            }
-        }
-        textView.setText(stringBuilder.toString());
-*/
         LinearLayoutManager layoutManager = new LinearLayoutManager(ReleaseActivity.this,LinearLayoutManager.HORIZONTAL,false);
         recyclerView2.setLayoutManager(layoutManager);
         recyclerView2.setItemAnimator(new DefaultItemAnimator());
@@ -70,6 +57,14 @@ public class ReleaseActivity extends MainActivity {
                         startActivity(new Intent(getApplicationContext(),MainActivity.class));
                         overridePendingTransition(0,0);
                         return true;
+                    case R.id.nav_likes:
+                        startActivity(new Intent(getApplicationContext(),Likes.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.nav_watchlist:
+                        startActivity(new Intent(getApplicationContext(),Watchlist.class));
+                        overridePendingTransition(0,0);
+                        return true;
                 }
                 return false;
             }
@@ -85,9 +80,7 @@ public class ReleaseActivity extends MainActivity {
             String line = null;
             while((line = br.readLine()) != null) {
                 String[] parte = line.split(";");
-                String mDrawableName = sb.append("release").append(l).toString();
-                int imageID = getResources().getIdentifier(mDrawableName , "drawable", getPackageName());
-                releaseList.add(new Movie(l,parte[0],imageID,parte[1],parte[2],parte[3],0,0,parte[4],parte[5]));
+                releaseList.add(new Movie(l,parte[0],parte[3],parte[1],parte[2],parte[4],0,"-",0,parte[4],parte[5]));
                 // read next line
                 sb = new StringBuilder();
                 l++;
